b9abb71a422a8e33f39490b0b99a9787de36ec92
Sarmad Amjad <sarmadamjad76@gmail.com>
2023-06-22 12:20:20 +0500

    Update script.js
    Created JS code for required check using simple JS

---
+++ p01/script.js
@@ -0,0 +1,48 @@
+const form = document.getElementById('form');
+const username = document.getElementById('username');
+const email = document.getElementById('email');
+const password = document.getElementById('password');
+const password2 = document.getElementById('password2');
+
+//All Functions
+//Funstion to show error
+function showError(input, message) {
+    const formControl = input.parentElement;
+    formControl.className = 'form-control error';
+    const small = formControl.querySelector('small');
+    small.innerText = message;
+}
+
+//Funstion to show success
+function showSuccess(input) {
+    const formControl = input.parentElement;
+    formControl.className = 'form-control success';
+}
+
+//This is an event listener for the form submit
+form.addEventListener('submit', function (e) {
+    e.preventDefault();
+    if (username.value === '') {
+        showError(username, 'Username is required')
+    } else {
+        showSuccess(username);
+    }
+
+    if (email.value === '') {
+        showError(email, 'email is required')
+    } else {
+        showSuccess(email);
+    }
+
+    if (password.value === '') {
+        showError(password, 'password is required')
+    } else {
+        showSuccess(password);
+    }
+
+    if (password2.value === '') {
+        showError(password2, 'Confirm password is required')
+    } else {
+        showSuccess(password2);
+    }
+})
